c710d6970152771252f0a92b04469437526232e9
Alvi Akbar <ala273@tux6-1.usask.ca>
2020-01-20 05:18:19 -0600

    Added more of my flavours to by base skeleton program

---
+++ base_project/tests/libbase_tests.c
@@ -0,0 +1,21 @@
+#include "minunit.h"
+#include <dlfcn.h>
+
+typedef int (*lib_function) (const char *data);
+char *lib_file = "build/libYOUR_LIBRARY.so";
+void *lib = NULL;
+
+char *test_dummy()
+{
+}
+
+char *all_tests()
+{
+    mu_suite_start();
+
+    mu_run_test(test_dummy);
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests);
