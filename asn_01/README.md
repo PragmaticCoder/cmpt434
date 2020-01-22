51e2c34ce4786dd1b300bd764eb16bbe0df8a13f
Alvi Akbar <ala273@tux5.usask.ca>
2020-01-22 14:04:43 -0600

    inherited my custom  base project

---
+++ asn_01/README.md
@@ -0,0 +1,6 @@
+# Base Project
+
+## CMPT 434: Computer Network
+
+* This base project is used as skeleton program for all assignments.
+* We are strictly following `C11` or `clang` format.
---
+++ asn_01/src/libbase.c
@@ -0,0 +1,12 @@
+#include "dbg.h"
+#include <ctype.h>
+#include <stdio.h>
+
+int print_a_message(const char *msg) {
+
+  printf("A STRING: %s\n", msg);
+  
+  return 0;
+}
+
+int fail_on_purpose(const char *msg) { return 1; }
