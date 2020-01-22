df8c74d7dd9af8c68d4b5216e9109272335f8377
Alvi Akbar <ala273@tux5.usask.ca>
2020-01-22 16:29:49 -0600

    included all socket libraries in my client v1 file

---
+++ asn_01/bin/client_v1.c
@@ -0,0 +1,16 @@
+#undef NDEBUG
+#include <stdio.h>
+#include <stdlib.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+
+#include<netinet/in.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+  printf("Reached main()\n");
+  return 0;
+}
