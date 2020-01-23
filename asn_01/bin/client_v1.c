72868c91baa7efffa3913bbbe7d17cef3e0cd2a3
Alvi Akbar <ala273@tux5.usask.ca>
2020-01-22 20:02:33 -0600

    checkpoint

--- asn_01/bin/client_v1.c
+++ asn_01/bin/client_v1.c
@@ -1,16 +1,26 @@
 #undef NDEBUG
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include<sys/types.h>
-#include<sys/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-#include<netinet/in.h>
+#include <netinet/in.h>
 
+int client_connect(char *host, char *port) {
 
+  int network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char const *argv[])
-{
+  return network_socket;
+}
+
+int main(int argc, char const *argv[]) {
+
+  int socket = 0;
+  // socket = client_connect();
+
   printf("Reached main()\n");
   return 0;
 }
