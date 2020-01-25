c12f0617fc7f2f9cb5b01c97e88f3262608c1403
Alvi Akbar <ala273@tux7.usask.ca>
2020-01-25 03:04:10 -0600

    changed clang format style to mozilla

--- asn_01/bin/client_v1.c
+++ asn_01/bin/client_v1.c
@@ -18,7 +18,8 @@
 
 #define BUFFSIZE 1024
 
-int connect_client(const char *host, const char *port)
+int
+connect_client(const char* host, const char* port)
 {
   int status = 0;
 
@@ -26,31 +27,31 @@ int connect_client(const char *host, const char *port)
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   check(sockfd >= 0, "Failed to created socket.");
 
-  /* specifying an destination address for the socket*/
+  /* specifying an destination address for the socket */
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
 
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = INADDR_ANY;
 
-  status =
-      connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  status = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
   check(status == -1, "Failed to create connection");
 
   return sockfd;
 
 error:
   freeaddrinfo(server_addr);
-  return -1;
+  return (-1);
 }
 
-int main(int argc, char const *argv[])
+int
+main(int argc, char const* argv[])
 {
 
   check(argc == 3, "USAGE: ./client_v1 <host> <port>");
   log_info("HOST: %s \tPORT: %s", argv[1], argv[2]);
 
-  /* creating connection to servers*/
+  /* creating connection to servers */
   int conn = 0;
   conn = connect_client(argv[1], argv[2]);
   check(conn >= 0, "Connection to Server %s:%s Failed", argv[1], argv[2]);
@@ -65,7 +66,7 @@ int main(int argc, char const *argv[])
   close(conn);
 
 error:
-  return -1;
+  return (-1);
 
-  return 0;
+  return (0);
 }
