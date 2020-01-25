3f42edc7e962497b63aaa779ae9553f9cf4dcd60
Alvi Akbar <ala273@tux7.usask.ca>
2020-01-25 13:36:59 -0600

    fixed the issue with sanity test script

--- asn_01/.gdbinit
+++ asn_01/.gdbinit
@@ -5,9 +5,18 @@ set pagination off
 set logging file output.txt
 set logging on
 
+
+
 set args 0.0.0.0 30000
+
+# For client
+# b main
+# b connect_client
+
+# For server
 b main
-b connect_client
+b setup_server
+
 r
 
 refresh
--- asn_01/sanity_test.sh
+++ asn_01/sanity_test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
 # This script is used to automate debugging
+
+# Setting Default
+PROGRAM="client_v1"
+
 usage() {
-  echo "Usage ${0} [-d][-v]" >&2
+  echo "Usage ${0} [-r][-d][-v] <client|server>" >&2
   echo 'Debug with automated tools'
   echo '-d Runs in debug mode with GDB.'
   echo '-v Runs in debug mode with Valgrind.'
@@ -11,27 +14,43 @@ usage() {
 
 debug_gdb() {
   echo 'Debugging with GDB'
-  gdb -x .gdbinit ./bin/client_v1
+  gdb -x .gdbinit ./bin/$1
   exit 0
 }
 
 debug_valgrind() {
   echo 'Debugging with Valgrind'
-  valgrind ./bin/client_v1 0.0.0.0 30000
+  valgrind ./bin/$1 0.0.0.0 30000
   exit 0
+}
+
+select_program() {
+  if [[ "${OPTARG}" = "server" ]]; then
+    PROGRAM="server_v1"
+  elif [[ "${OPTARG}" = "client" ]]; then
+    PROGRAM="client_v1"
+  fi
 }
 
 make clean && clear && make
 
 echo 'Running Sanity Test'
 
-while getopts dv OPTION; do
+while getopts "r:d:v:" OPTION; do
   case ${OPTION} in
+  r)
+    echo "selected - RUN MODE"
+    select_program
+    ;;
   d)
-    debug_gdb
+    echo "GDB DEBUG MODE"
+    select_program
+    debug_gdb $PROGRAM
     ;;
   v)
-    debug_valgrind
+    echo "VALGRIND DEBUG MODE"
+    select_program
+    debug_valgrind $PROGRAM
     ;;
   ?)
     usage
@@ -39,5 +58,6 @@ while getopts dv OPTION; do
   esac
 done
 
-./bin/client_v1 0.0.0.0 30000
+./bin/$PROGRAM 0.0.0.0 30000
+
 exit 0
