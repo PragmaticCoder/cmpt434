ec55e4d83557fb0fb133d097dab5e33325e975ea
Alvi Akbar <ala273@tux7.usask.ca>
2020-01-26 01:39:30 -0600

    code refactoring and clean up forr client_v1.c

--- asn_01/bin/client_v1.c
+++ asn_01/bin/client_v1.c
@@ -35,6 +35,34 @@ chat(int sockfd)
 }
 
 int
+connect_client(const char* host, const char* port)
+{
+  /* Socket creation */
+  int sockfd = 0;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  check(sockfd != -1, "Socket creation failed!");
+
+  /* Specify destination address */
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(host);
+  server_addr.sin_port = port;
+
+  /* Establish connection client & server socket */
+  int status = 0;
+  status = connect(sockfd, (SA*)&server_addr, sizeof(server_addr));
+  check(status == 0, "Connection to server failed ...");
+
+  return sockfd;
+
+error:
+  freeaddrinfo(server_addr);
+  return (-1);
+}
+
+int
 main(int argc, char const* argv[])
 {
 
@@ -42,21 +70,8 @@ main(int argc, char const* argv[])
   log_info("HOST: %s \tPORT: %s", argv[1], argv[2]);
 
   int sockfd = 0;
-  struct sockaddr_in servaddr;
-
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  check(sockfd != -1, "Socket creation failed!");
-
-  bzero(&servaddr, sizeof(servaddr));
-
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-  servaddr.sin_port = argv[2];
-
-  /* connect client socket to server socket */
-  int status = 0;
-  status = connect(sockfd, (SA*)&servaddr, sizeof(servaddr));
-  check(status == 0, "Connection to server failed ...");
+  sockfd = connect_client(argv[1], argv[2]);
+  check(sockfd >= 0, "Connection to Server %s:%s Failed", argv[1], argv[2]);
 
   chat(sockfd);
 
