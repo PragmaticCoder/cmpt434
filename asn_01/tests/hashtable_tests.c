7420c21123e4d10f0596ba7106215646ee6bf8f9
Alvi Akbar <ala273@tux5.usask.ca>
2020-01-28 17:12:20 -0600

    got hashtable put test to work properly xD - Loving my test suite

--- asn_01/tests/hashtable_tests.c
+++ asn_01/tests/hashtable_tests.c
@@ -1,10 +1,20 @@
-#include "minunit.h"
-#include <dlfcn.h>
+#undef NDEBUG
 
-typedef int (*Hashtable_get)(char* data);
-typedef int (*Hashtable_put)(char* name, char* value);
+#include "minunit.h"
+#include "hashtable.h"
+
+#include <dlfcn.h>
+#include <string.h>
+
+typedef struct HashNode HashNode_t;
+
+typedef HashNode_t* (*lib_Hashtable_get)(const char* data);
+typedef HashNode_t* (*lib_Hashtable_put)(const char* key, const char* value);
+
 char* lib_file = "./build/libasn_01.so";
 void* lib = NULL;
+
+HashNode_t* node;
 
 char*
 test_dlopen()
@@ -16,11 +26,31 @@ test_dlopen()
 }
 
 char*
+test_put_success()
+{
+
+  lib_Hashtable_put func_Hashtable_put = dlsym(lib, "Hashtable_put");
+  mu_assert(func_Hashtable_put != NULL, "Failed to create Hash Node.");
+
+  char* key = "name";
+  char * value = "Alvi";
+
+  HashNode_t* node = func_Hashtable_put("name", "Alvi");
+  mu_assert(node != NULL, "Failed to put (key, value) pair into hashtable");
+  
+  mu_assert(strcmp(node->name, key) == 0, "Failed: HashNode key mismatch");
+  mu_assert(strcmp(node->value, value) == 0, "Failed: HashNode value mismatch");
+
+  return NULL;
+}
+
+char*
 all_tests()
 {
   mu_suite_start();
 
   mu_run_test(test_dlopen);
+  mu_run_test(test_put_success);
 
   return NULL;
 }
