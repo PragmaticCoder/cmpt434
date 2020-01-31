f4bb5dc66c6137c334a4c89bedc73a11fe46cfaf
Alvi Akbar <ala273@tux8.usask.ca>
2020-01-31 09:05:46 -0600

    added dictionary implementation

---
+++ asn_01/bin/dictionary.h
@@ -0,0 +1,28 @@
+#ifndef __dictionary_h__
+#define __dictionary_h__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _dictionary
+{
+  char* key;
+  char* value;
+  struct _dictionary* next;
+  struct _dictionary* prev;
+} dictionary_t;
+
+int
+Dictionary_add(char* key, char* value);
+int
+Dictionary_getValue(char* key, char* value);
+int
+Dictionary_getAll(char* key, char* value);
+int
+Dictionary_remove(char* key);
+
+int
+Command_interface(int argc, char* argv[], char* retBuffer);
+
+#endif
