ebd1eeb974c308608f26c617fb8847267cd8428b
Alvi Akbar <ala273@tux8.usask.ca>
2020-01-31 03:39:52 -0600

    code refactoring completed for udp_tcp_proxy.c

--- asn_01/bin/udp_tcp_proxy.c
+++ asn_01/bin/udp_tcp_proxy.c
@@ -11,7 +11,65 @@
 struct sockaddr_in clientSock;
 
 void
-HandleClient(int client, int server);
+Handle_client(int client, int server)
+{
+  int fromSize;
+  int recvLength;
+  int sentLength;
+  char buffer[100];
+  struct sockaddr_in fromSock;
+
+  while (1) {
+
+    if ((recvLength = recv(client, buffer, sizeof(buffer) - 1, 0)) < 0) {
+      printf("Error in Receving the Character from TCP client\n");
+      break;
+    }
+
+    if (recvLength == 0) {
+      printf("client lost connection\n");
+      break;
+    }
+    buffer[recvLength] = 0;
+    printf("Received TCP Data : %s\n", buffer);
+
+    if ((sentLength = sendto(server,
+                             buffer,
+                             recvLength,
+                             0,
+                             (struct sockaddr*)&clientSock,
+                             sizeof(struct sockaddr))) != recvLength) {
+      printf("Error in Sending data to UDP client\n");
+      break;
+    }
+
+    fromSize = sizeof(struct sockaddr);
+    if ((recvLength = recvfrom(server,
+                                    buffer,
+                                    sizeof(buffer) - 1,
+                                    0,
+                                    (struct sockaddr*)&fromSock,
+                                    &fromSize)) < 0) {
+      printf("Error in Receving the Character from UDP server\n");
+      break;
+    }
+
+    buffer[recvLength] = 0;
+    printf("Received UPD Data : %s\n", buffer);
+
+    printf("Received TCP Data from Server : %s\n", buffer);
+
+    strcat(buffer, " - from the Proxy");
+    recvLength = strlen(buffer);
+
+    if ((sentLength = send(client, buffer, recvLength, 0)) !=
+        recvLength) {
+      printf("Error in Sending data to TCP client\n");
+      break;
+    }
+  }
+  close(client);
+}
 
 int
 main(int argc, char* argv[])
@@ -42,99 +100,31 @@ main(int argc, char* argv[])
   serverSock.sin_addr.s_addr = htonl(INADDR_ANY);
   serverSock.sin_port = htons(atoi(argv[3]));
 
-  if (bind(serverFD,
-           (struct sockaddr*)&serverSock,
-           sizeof(struct sockaddr_in)) < 0) {
-    printf("Error in binding the Socket connection of TCP Server\n");
-    exit(1);
-  }
-  if (listen(serverFD, MAXPENDING) < 0) {
-    printf("Error in listing the Socket connection of TCP Server\n");
-    exit(1);
-  }
+  check((bind(serverFD,
+              (struct sockaddr*)&serverSock,
+              sizeof(struct sockaddr_in)) >= 0),
+        "Error in binding the Socket connection of TCP Server");
+
+  check(listen(serverFD, MAXPENDING) >= 0,
+        "Error in listing the Socket connection of TCP Server");
 
   while (1) {
 
-    if ((tcpClientFD =
-           accept(serverFD, (struct sockaddr*)&tcpClientSock, &fromSize)) < 0) {
-      printf("Error in Accepting the Socket connection of TCP Server\n");
-      exit(1);
-    }
-    printf("IP : %s Port %d\n\n",
-           inet_ntoa(tcpClientSock.sin_addr),
-           ntohs(tcpClientSock.sin_port));
-    HandleClient(tcpClientFD, clientFD);
+    check(((tcpClientFD = accept(
+              serverFD, (struct sockaddr*)&tcpClientSock, &fromSize)) >= 0),
+          "Error in Accepting the Socket connection of TCP Server");
+
+    log_info("IP : %s Port %d\n\n",
+             inet_ntoa(tcpClientSock.sin_addr),
+             ntohs(tcpClientSock.sin_port));
+
+    Handle_client(tcpClientFD, clientFD);
   }
 
   close(clientFD);
 
+  return 0;
+
 error:
-  if (clientFD)
-    close(clientFD);
-
-  if (serverFD)
-    close(serverFD);
-
   return (-1);
-}
-
-void
-HandleClient(int client, int server)
-{
-  int fromSize;
-  int RecevicedLength;
-  int sentLength;
-  char buffer[100];
-  struct sockaddr_in fromSock;
-
-  while (1) {
-
-    if ((RecevicedLength = recv(client, buffer, sizeof(buffer) - 1, 0)) < 0) {
-      printf("Error in Receving the Character from TCP client\n");
-      break;
-    }
-
-    if (RecevicedLength == 0) {
-      printf("client lost connection\n");
-      break;
-    }
-    buffer[RecevicedLength] = 0;
-    printf("Received TCP Data : %s\n", buffer);
-
-    if ((sentLength = sendto(server,
-                             buffer,
-                             RecevicedLength,
-                             0,
-                             (struct sockaddr*)&clientSock,
-                             sizeof(struct sockaddr))) != RecevicedLength) {
-      printf("Error in Sending data to UDP client\n");
-      break;
-    }
-
-    fromSize = sizeof(struct sockaddr);
-    if ((RecevicedLength = recvfrom(server,
-                                    buffer,
-                                    sizeof(buffer) - 1,
-                                    0,
-                                    (struct sockaddr*)&fromSock,
-                                    &fromSize)) < 0) {
-      printf("Error in Receving the Character from UDP server\n");
-      break;
-    }
-
-    buffer[RecevicedLength] = 0;
-    printf("Received UPD Data : %s\n", buffer);
-
-    printf("Received TCP Data from Server : %s\n", buffer);
-
-    strcat(buffer, " - from the Proxy");
-    RecevicedLength = strlen(buffer);
-
-    if ((sentLength = send(client, buffer, RecevicedLength, 0)) !=
-        RecevicedLength) {
-      printf("Error in Sending data to TCP client\n");
-      break;
-    }
-  }
-  close(client);
 }
