ab95043482980e917389e93c4150ca2fb9dca799
Pheonix Hax <anonymousanon@localhost.localdomain>
2020-02-11 18:47:37 -0600

    added get tail test

--- a2/tests/frame_tests.c
+++ a2/tests/frame_tests.c
@@ -3,58 +3,58 @@
  * NSID: ala273
  * Student Number: 11118887
  */
-#undef NDEBUG
+
 #include "minunit.h"
 #include <frame.h>
 #include <string.h>
 
-
-char*
+char *
 test_first_frame_add_success()
-{   
-    frame_t *frame;
-    frame = Frame_add("101");
+{
+  frame_t *frame;
+  frame = Frame_add("100");
 
-    mu_assert(frame != NULL, "Failed to add a Frame.");
-    mu_assert(frame->index == 0, "Frame index is not zero.");
+  mu_assert(frame != NULL, "Failed to add a Frame.");
+  mu_assert(frame->index == 0, "Frame index is not zero.");
 
-    debug("%s\n", frame->message);
-    return NULL;
+  return NULL;
 }
 
-char*
+char *
 test_frame_add_correct_sequence()
 {
-    frame_t *frame;
-    frame = Frame_add("110");
+  frame_t *frame;
+  frame = Frame_add("101");
 
-    mu_assert(frame != NULL, "Failed to add a Frame.");
-    mu_assert(strncmp(frame->message, "110", 3) == 0, "Wrong frame message returned.")
-    mu_assert(frame->index == 1, "Wrong index returned.");
+  mu_assert(frame != NULL, "Failed to add a Frame.");
+  mu_assert(strncmp(frame->message, "101", 3) == 0, "Wrong frame message returned.");
+  mu_assert(frame->index == 1, "Wrong index returned.");
 
-    return NULL;
+  return NULL;
 }
 
-char*
-test_frame_add_success()
+char *
+test_get_tail()
 {
-    frame_t *frame;
-    frame = Frame_add("101");
+  frame_t *frame;
+  frame = Get_tail();
 
-    mu_assert(frame != NULL, "Failed to create frame.");
-    mu_assert(frame->index == 1, "Wrong index returned.");
+  mu_assert(frame != NULL, "No frame available on tail.");
+  mu_assert(strncmp(frame->message, "100", 3) == 0, "Wrong frame message returned.");
+  mu_assert(frame->index == 0, "Incorrect frame index.");
 
-    return NULL;
+  return NULL;
 }
 
-
-char*
+char *
 all_tests()
 {
   mu_suite_start();
 
   mu_run_test(test_first_frame_add_success);
   mu_run_test(test_frame_add_correct_sequence);
+
+  mu_run_test(test_get_tail);
 
   return NULL;
 }
