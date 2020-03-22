e6562c528266ec37f57532f3a8c51c762f08c97b
Alvi Akbar <ala273@tux5.usask.ca>
2020-03-22 07:44:05 -0600

    minor fix

--- a3/tests/libasn_03_tests.c
+++ a3/tests/libasn_03_tests.c
@@ -4,8 +4,8 @@
  * Student Number: 11118887
  */
 
+#include "libasn_03.h"
 #include "minunit.h"
-#include "libasn_02.h"
 
 char*
 test_dummy()
@@ -13,11 +13,12 @@ test_dummy()
   return NULL;
 }
 
-char *test_print_a_message()
+char*
+test_print_a_message()
 {
-    int rc = print_a_message("Printing Tests");
-    mu_assert(rc == 0, "Failed to execute print_a_message.");
-    return NULL;
+  int rc = print_a_message("Printing Tests");
+  mu_assert(rc == 0, "Failed to execute print_a_message.");
+  return NULL;
 }
 
 char*
